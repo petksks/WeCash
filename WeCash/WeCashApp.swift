6695e9767011572c3201cb6fcd1b9a05ec651327
Peter Sederblad <petersederblad@Peters-MacBook-Air.local>
2023-09-22 12:58:57 +0200

    initial commit

---
+++ WeCash/WeCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeCashApp.swift
+//  WeCash
+//
+//  Created by Peter Sederblad on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct WeCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
